f0d809b697e7a28599768f2d24e8754978bd5d5f
abcdeclair <abcdeclair@gmail.com>
2022-07-06 17:29:46 +0700

    change path .env

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import express from "express";
 import convert from "./routes/convert";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
-dotenv.config();
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 const PORT = process.env.PORT ?? 3333;
@@ -16,6 +18,6 @@ app.get("/", (req, res) => {
 
 let listener = app.listen(PORT, () => {
   const addr = listener.address() as AddressInfo;
-  console.log(`server listening on port` , addr.port);
+  console.log(`server listening on port`, addr.port);
   console.log(addr.port);
 });
