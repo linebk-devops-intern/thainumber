3af42a25022a03cebb26e7f2e6ab45db05de819d
phongsakorn <phongsakorn552@gmail.com>
2022-08-19 12:29:18 +0700

    refractoring

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,37 @@ import React,{useState} from 'react';
 import './app.css';
 
 const baseURL = "http://localhost:3333/main/thainumber"
+
 function App() {
+
   let [posts,setPosts] = useState([null]);
   let [userint,setUserint] = useState("1212");
+  
   const handleSubmit = (e) => {
     e.preventDefault();
  };
+
   const  tranform = (e) =>{
+
      axios.post(baseURL,{
       "input": userint
     }).then((response) => {
       setPosts(response.data); 
+      
     })
   }
 
   return (
-    <div onSubmit={handleSubmit}>
-      <input type = "text" value ={userint}  onChange={e =>setUserint(e.target.value)}  />
-      <button type='button' class='button' onClick={tranform}>Tranfer</button>   
-      <br></br>{posts.thaiNum}  
-      <br></br>{posts.text}  
-      <br></br>{posts.currencyText}
+    
+    <div className='box' onSubmit={handleSubmit}>
+
+      <input className='input' type = "text" value = {userint}  onChange={e => setUserint(e.target.value)}  />
+
+       &nbsp;<button type= 'button' className = 'button' onClick = {tranform}>Tranfer</button> 
+         
+       &nbsp;<br></br><h4>thainumber  &nbsp;</h4>{posts.thaiNum} 
+      <br></br><h4>&nbsp;text  &nbsp;</h4>{posts.text}  
+      <br></br><h4>&nbsp;currencyText  &nbsp;</h4>{posts.currencyText}
     </div>
   );
 } 
